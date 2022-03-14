13badbc57e53f6589a629f8dc9b87ed571a72e18
Juhyung Park <qkrwngud825@gmail.com>
2022-03-15 01:20:42 +0900

    msm8998-common: enable KERNEL_CUSTOM_LLVM
    Change-Id: I9824f54f5b7942cd65a2536b9d7c8dbf8b11ae19
    Signed-off-by: Juhyung Park <qkrwngud825@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,6 +42,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_VERSION := 4.4
+KERNEL_CUSTOM_LLVM := true
 
 BOARD_KERNEL_CMDLINE := androidboot.console=ttyMSM0 androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
