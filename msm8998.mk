6a9d4d02ac97dc1134b6bdcd8d03948f2000d196
LynnrinChan <lynnrin@lynnrin.moe>
2023-02-09 16:50:02 +0800

    msm8998-common: overlay: Disable VoWiFi for China carrier
    Change-Id: I05f329665e748246466357e8f7cf7b83a71e9307

--- msm8998.mk
+++ msm8998.mk
@@ -366,7 +366,6 @@ PRODUCT_ODM_PROPERTIES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.wfc_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1
 
 PRODUCT_COPY_FILES += \
